58ad5e9c848f079aecd39f6be146e5f1ca46b70d
Nicolas Bloj <nicolasbloj@gmail.com>
2017-09-20 19:05:05 -0300

    helloworld controller

---
+++ src/main/java/com/nab/mayco/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.nab.mayco.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+  @RequestMapping("/")
+  public String hello() {
+    return "Hello World ...";
+  }
+
+  @RequestMapping("/bye")
+  public String goodBye() {
+    return "Goodbye World ...";
+  }
+
+}
