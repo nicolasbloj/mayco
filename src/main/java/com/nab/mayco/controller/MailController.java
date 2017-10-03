913516e19dd1f0681ca54bb04dae735039ae87c6
Nicolas Bloj <nicolasbloj@gmail.com>
2017-10-03 19:05:34 -0300

    cors origin

--- src/main/java/com/nab/mayco/controller/MailController.java
+++ src/main/java/com/nab/mayco/controller/MailController.java
@@ -3,6 +3,7 @@ package com.nab.mayco.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import com.nab.mayco.service.MailService;
 
 @RestController
 @RequestMapping("/mail")
+@CrossOrigin
 public class MailController {
 
   @Autowired
